38ba0beabda12c8b9bfa3a97b1af2409580f1f3b
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-02-22 11:25:46 +0000

    created quicksort class files

---
+++ P_04/include/quicksort.h
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file quicksort.h
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of the class QuickSort.
+ * @version 0.1
+ * @since Feb 22 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+// TODO: Implement the QuickSort class
+class QuickSort {
+};
---
+++ P_04/src/quicksort.cc
@@ -0,0 +1,17 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file quicksort.cc
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Definition of the class QuickSort.
+ * @version 0.1
+ * @since Feb 22 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../include/quicksort.h"
