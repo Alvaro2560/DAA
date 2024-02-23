e4f196fc7ed3143b6a6e13c3020ecf3b4a83080b
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-02-23 11:18:53 +0000

    main created

---
+++ P_04/src/main.cc
@@ -0,0 +1,25 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file main.cc
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Main file of the program.
+ * @version 0.1
+ * @since Feb 23 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  try {
+    return 0;
+  } catch(const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+}
