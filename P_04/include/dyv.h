e54d3f2d276e60c06da7ab386afe3476b9f7c48c
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-02-23 17:07:51 +0000

    library

---
+++ P_04/include/dyv.h
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file functions.cc
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Namespace of the framework.
+ * @version 0.1
+ * @since Feb 23 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include "quicksort.h"
+#include "mergesort.h"
+#include "algorithm.h"
