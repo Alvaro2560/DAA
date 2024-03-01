d6bc653af4414098550232799d50c207d36c402d
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-01 18:34:55 +0000

    functions finished

--- P_04/src/functions.cc
+++ P_04/src/functions.cc
@@ -16,18 +16,14 @@
 
 #include "../include/functions.h"
 
-#include <iostream>
-#include <stdlib.h>
 #include <time.h>
-#include <fstream>
-#include <chrono>
 
 /**
  * @brief Prints the help message.
  * 
  */
 void Help(void) {
-  std::cout << "Usage: ./dyv [arrays]" << std::endl;
+  std::cout << "Usage: ./dyv [arrays] [-f]" << std::endl;
   std::cout << "Generates a random set of arrays and sorts them using Quick Sort and Merge Sort." << std::endl;
 }
 
@@ -47,78 +43,24 @@ std::vector<int> GenerateRandom(const size_t& size) {
 }
 
 /**
- * @brief Calculates the time it takes to solve the problems.
+ * @brief Prints the information of the times it took to solve the problems.
  * 
- * @param array The array to solve.
- * @param quicksort The Quick Sort algorithm.
- * @param mergesort The Merge Sort algorithm.
- * @param quicksort_solutions The solutions to the Quick Sort problem.
- * @param mergesort_solutions The solutions to the Merge Sort problem.
- * @param quicksort_times The time it took to solve the Quick Sort problem.
- * @param mergesort_times The time it took to solve the Merge Sort problem.
+ * @param quicksort_times Times it took to solve the problems using Quick Sort.
+ * @param mergesort_times Times it took to solve the problems using Merge Sort.
  */
-void CalculateTime(const std::vector<int>& array, DyV::QuickSort& quicksort,
-                   DyV::MergeSort& mergesort, std::vector<std::vector<int>>& quicksort_solutions,
-                   std::vector<std::vector<int>>& mergesort_solutions, std::vector<float>& quicksort_times,
-                   std::vector<float>& mergesort_times) {
-  std::chrono::time_point<std::chrono::system_clock> start, end;
-  start = std::chrono::system_clock::now();
-  quicksort_solutions.emplace_back(quicksort.Solve(array, array.size()));
-  end = std::chrono::system_clock::now();
-  quicksort_times.emplace_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
-  start = std::chrono::system_clock::now();
-  mergesort_solutions.emplace_back(mergesort.Solve(array, array.size()));
-  end = std::chrono::system_clock::now();
-  mergesort_times.emplace_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
-}
-
-/**
- * @brief Prints the data to the console.
- * 
- * @param quicksort_solutions The solutions to the Quick Sort problem.
- * @param mergesort_solutions The solutions to the Merge Sort problem.
- * @param quicksort_times The time it took to solve the Quick Sort problem.
- * @param mergesort_times The time it took to solve the Merge Sort problem.
- */
-void PrintData(std::vector<std::vector<int>>& quicksort_solutions,
-               std::vector<std::vector<int>>& mergesort_solutions,
-               std::vector<float>& quicksort_times,
-               std::vector<float>& mergesort_times) {
-  for (size_t i = 0; i < quicksort_solutions.size(); i++) {
-    std::cout << "Quick Sort:\n";
-    std::cout << "Size: " << quicksort_solutions[i].size() << ", ";
-    std::cout << "Time: " << quicksort_times[i] << "ms" << std::endl;
+void PrintInfo(const std::vector<std::pair<size_t, float>>& quicksort_times,
+               const std::vector<std::pair<size_t, float>>& mergesort_times) {
+  std::cout << "QuickSort times:" << std::endl << std::endl;
+  std::cout << "Size             Time (ms)" << std::endl;
+  std::cout << "----------------------------" << std::endl;
+  for (size_t i = 0; i < quicksort_times.size(); i++) {
+    std::cout << quicksort_times[i].first << "                 " << quicksort_times[i].second << std::endl;
   }
-  for (size_t i = 0; i < mergesort_solutions.size(); i++) {
-    std::cout << "Merge Sort:\n";
-    std::cout << "Size: " << mergesort_solutions[i].size() << ", ";
-    std::cout << "Time: " << mergesort_times[i] << "ms" << std::endl;
+  std::cout << std::endl;
+  std::cout << "MergeSort times:" << std::endl << std::endl;
+  std::cout << "Size             Time (ms)" << std::endl;
+  std::cout << "----------------------------" << std::endl;
+  for (size_t i = 0; i < mergesort_times.size(); i++) {
+    std::cout << mergesort_times[i].first << "                 " << mergesort_times[i].second << std::endl;
   }
-}
-
-/**
- * @brief Writes the data to a file.
- * 
- * @param quicksort_solutions The solutions to the Quick Sort algorithm.
- * @param mergesort_solutions The solutions to the Merge Sort algorithm.
- * @param quicksort_times The times it took to solve the problems using Quick Sort.
- * @param mergesort_times The times it took to solve the problems using Merge Sort.
- */
-void WriteData(std::vector<std::vector<int>>& quicksort_solutions,
-               std::vector<std::vector<int>>& mergesort_solutions,
-               std::vector<float>& quicksort_times,
-               std::vector<float>& mergesort_times) {
-  std::ofstream file;
-  file.open("quick_sort.txt");
-  for (size_t i = 0; i < quicksort_solutions.size(); i++) {
-    file << "Size: " << quicksort_solutions[i].size() << ", ";
-    file << "Time: " << quicksort_times[i] << "ms" << std::endl;
-  }
-  file.close();
-  file.open("merge_sort.txt");
-  for (size_t i = 0; i < mergesort_solutions.size(); i++) {
-    file << "Size: " << mergesort_solutions[i].size() << ", ";
-    file << "Time: " << mergesort_times[i] << "ms" << std::endl;
-  }
-  file.close();
-}
+} 
