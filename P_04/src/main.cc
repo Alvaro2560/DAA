74486435cd59b5a5e4243091eb1e470a6d589d7f
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-01 18:35:08 +0000

    main finished

--- P_04/src/main.cc
+++ P_04/src/main.cc
@@ -17,26 +17,45 @@
 #include "../include/functions.h"
 
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char** argv) {
   try {
-    if (argc != 2) {
+    std::ofstream quicksort_file, mergesort_file;
+    if ((argc == 3 && std::string(argv[2]) != "-f") || 
+        (argc == 2 && std::string(argv[1]) == "-h") || (argc > 3) || (argc < 2)) {
       Help();
       return 1;
+    } else if (argc == 3 && std::string(argv[2]) == "-f") {
+      quicksort_file.open("quicksort_times.csv");
+      mergesort_file.open("mergesort_times.csv");
     }
-    std::vector<std::vector<int>> instances(std::stoi(argv[1]));
-    for (size_t i = 0, j = 2; i < instances.size(); i++, j++) {
-      instances[i] = GenerateRandom(j);
+    const size_t kInstances = (size_t)std::stoi(argv[1]);
+    const size_t kPrintableIndex = kInstances / 10;
+    size_t printable_element = kPrintableIndex;
+    std::vector<std::pair<size_t, float>> quicksort_times, mergesort_times;
+    std::vector<int> instance;
+    float quicksort_time, mergesort_time;
+    for (size_t i = 2; i < kInstances + 1; i++) {
+      instance = GenerateRandom(i);
+      DyV::Problem<std::vector<int>> problem(instance);
+      quicksort_time = QuickSortTime(problem);
+      mergesort_time = MergeSortTime(problem);
+      if (i == printable_element) {
+        quicksort_times.emplace_back(std::pair<size_t, float>(i, quicksort_time));
+        mergesort_times.emplace_back(std::pair<size_t, float>(i, mergesort_time));
+        printable_element += kPrintableIndex;
+      }
+      if (quicksort_file.is_open()) {
+        quicksort_file << i << "," << quicksort_time << std::endl;
+        mergesort_file << i << "," << mergesort_time << std::endl;
+      }
     }
-    DyV::QuickSort quicksort;
-    DyV::MergeSort mergesort;
-    std::vector<std::vector<int>> quicksort_solutions, mergesort_solutions;
-    std::vector<float> quicksort_times, mergesort_times;
-    for (size_t i = 0; i < instances.size(); i++) {
-      CalculateTime(instances[i], quicksort, mergesort, quicksort_solutions, mergesort_solutions, quicksort_times, mergesort_times);
+    if (quicksort_file.is_open()) {
+      quicksort_file.close();
+      mergesort_file.close();
     }
-    PrintData(quicksort_solutions, mergesort_solutions, quicksort_times, mergesort_times);
-    WriteData(quicksort_solutions, mergesort_solutions, quicksort_times, mergesort_times);
+    PrintInfo(quicksort_times, mergesort_times);
     return 0;
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
