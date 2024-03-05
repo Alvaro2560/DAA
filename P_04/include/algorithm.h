094defea85b3a3cd5c6a7d36ab2fe3fd918abe3e
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-05 13:02:56 +0000

    modification

--- P_04/include/algorithm.h
+++ P_04/include/algorithm.h
@@ -34,18 +34,25 @@ class Algorithm {
      * 
      * @param array The problem to solve.
      * @param size The size of the problem.
+     * @param recursion_level Level of the recursion tree.
      * @return DyV::Solution<T> The solution to the problem.
      */
-    DyV::Solution<T> Solve(const DyV::Problem<T>& array, const size_t& size) {
+    DyV::Solution<T> Solve(const DyV::Problem<T>& array, const size_t& size, int recursion_level = 0) {
+      total_calls_++;
+      if (recursion_level > max_depth_) {
+        max_depth_ = recursion_level;
+      }
       if (Small(array)) {
         return SolveSmall(array);
       } else {
         std::vector<DyV::Problem<T>> subproblems = Divide(array, size);
         std::vector<DyV::Solution<T>> solutions;
+        // Solve the subproblems.
         for (size_t i = 0; i < subproblems.size(); i++) {
-          solutions.emplace_back(DyV::Solution<T>(Solve(subproblems[i], subproblems[i].size())));
+          solutions.emplace_back(DyV::Solution<T>(Solve(subproblems[i], subproblems[i].size(), recursion_level + 1)));
         }
         DyV::Solution<T> solution = solutions[0];
+        // Combine the solutions.
         for (size_t i = 1; i < solutions.size(); i++) {
           solution = Combine(solution, solutions[i]);
         }
@@ -67,6 +74,24 @@ class Algorithm {
       recurrence += c_;
       return recurrence;
     }
+
+    /**
+     * @brief Returns the maximum recursion level of the algorithm.
+     * 
+     * @return int The maximum recursion level of the algorithm.
+     */
+    int getMaxRecursionLevel(void) {
+      return max_depth_;
+    }
+
+    /**
+     * @brief Returns the total calls of the algorithm.
+     * 
+     * @return int The total calls of the algorithm.
+     */
+    int getTotalCalls(void) {
+      return total_calls_;
+    }
   protected:
     virtual bool Small(const DyV::Problem<T>& array) = 0;
     virtual DyV::Solution<T> SolveSmall(const DyV::Problem<T>& array) = 0;
@@ -76,4 +101,6 @@ class Algorithm {
     std::string a_;
     std::string b_;
     std::string c_;
+    int max_depth_;
+    int total_calls_;
 };
