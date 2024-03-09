d36b92ddae4aed3281d08a493dbf7bcf7839f652
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-09 13:54:24 +0000

    added template types for key and solution

--- P_05/include/algorithm.h
+++ P_05/include/algorithm.h
@@ -26,7 +26,7 @@
  * @brief Class that acts as a base for the Divide and Conquer algorithms.
  * 
  */
-template <typename T>
+template <typename T, typename U, typename V>
 class Algorithm {
   public:
     /**
@@ -36,17 +36,17 @@ class Algorithm {
      * @param size The size of the problem.
      * @return DyV::Solution<T> The solution to the problem.
      */
-    DyV::Solution<T> Solve(const DyV::Problem<T>& array, const size_t& size) {
+    DyV::Solution<V> Solve(const DyV::Problem<T>& array, U key = 0) {
       if (Small(array)) {
-        return SolveSmall(array);
+        return SolveSmall(array, key);
       } else {
-        std::vector<DyV::Problem<T>> subproblems = Divide(array, size);
-        std::vector<DyV::Solution<T>> solutions;
+        std::vector<DyV::Problem<T>> subproblems = Divide(array, key);
+        std::vector<DyV::Solution<V>> solutions;
         // Solve the subproblems.
         for (size_t i = 0; i < subproblems.size(); i++) {
-          solutions.emplace_back(DyV::Solution<T>(Solve(subproblems[i], subproblems[i].size()));
+          solutions.emplace_back(DyV::Solution<V>(Solve(subproblems[i], subproblems[i].size())));
         }
-        DyV::Solution<T> solution = solutions[0];
+        DyV::Solution<V> solution = solutions[0];
         // Combine the solutions.
         for (size_t i = 1; i < solutions.size(); i++) {
           solution = Combine(solution, solutions[i]);
@@ -71,9 +71,9 @@ class Algorithm {
     }
   protected:
     virtual bool Small(const DyV::Problem<T>& array) = 0;
-    virtual DyV::Solution<T> SolveSmall(const DyV::Problem<T>& array) = 0;
-    virtual std::vector<DyV::Problem<T>> Divide(const DyV::Problem<T>& array, const size_t& size) = 0;
-    virtual DyV::Solution<T> Combine(const DyV::Solution<T>& solution1, const DyV::Solution<T>& solution2) = 0;
+    virtual DyV::Solution<V> SolveSmall(const DyV::Problem<T>& array, const U& key = 0) = 0;
+    virtual std::vector<DyV::Problem<T>> Divide(const DyV::Problem<T>& array, const U& key = 0) = 0;
+    virtual DyV::Solution<V> Combine(const DyV::Solution<V>& solution1, const DyV::Solution<V>& solution2) = 0;
   protected:
     std::string a_;
     std::string b_;
