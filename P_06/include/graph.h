4bae1f8099b5f4aba9b36c5bc3dec45123e75027
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-14 09:41:47 +0000

    file created

---
+++ P_06/include/graph.h
@@ -0,0 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file graph.h
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of the class Graph.
+ * @version 0.1
+ * @since Mar 14 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include "node.h"
+
+class Graph {
+
+};
