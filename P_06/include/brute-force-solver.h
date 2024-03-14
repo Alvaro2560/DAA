5928f7695a38113c8bf9f701a954052f7a9a49f9
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-14 18:56:18 +0000

    done

---
+++ P_06/include/brute-force-solver.h
@@ -0,0 +1,27 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file brute-force-solver.h
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of the class BruteForce.
+ * @version 0.1
+ * @since Mar 14 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include "solver.h"
+
+class BruteForce : public Solver {
+  public:
+    BruteForce(void) = default;
+    ~BruteForce(void) = default;
+  private:
+    void RunAlgorithm(Graph* graph) override;
+};
