b04aa6e50ebc8affc3315b97b2fa8eea18805189
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-14 09:41:24 +0000

    file created

---
+++ P_06/include/solver.h
@@ -0,0 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file solver.h
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of the class Solver.
+ * @version 0.1
+ * @since Mar 14 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include "graph.h"
+
+class Solver {
+  
+};
