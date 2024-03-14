6ecebc986a475b5cffd0aea56ef9f900c554eb60
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-14 19:00:01 +0000

    file creation

---
+++ P_06/src/brute-force.cc
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file brute-force-solver.cc
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Definition of the class BruteForce.
+ * @version 0.1
+ * @since Mar 14 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../include/brute-force-solver.h"
+
+void BruteForce::RunAlgorithm(Graph* graph) {
+  
+}
