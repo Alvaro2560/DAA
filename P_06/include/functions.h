506084515cced474f7714004b49068107ac644a1
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-14 18:57:08 +0000

    first functions

---
+++ P_06/include/functions.h
@@ -0,0 +1,25 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file functions.h
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of the functions used on the program.
+ * @version 0.1
+ * @since Mar 14 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include "graph.h"
+
+#include <vector>
+#include <unordered_map>
+
+std::vector<std::string> ReadFile(const std::string& file_name);
+Graph* CreateGraph(const std::vector<std::string>& file_content);
