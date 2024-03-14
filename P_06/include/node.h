52714a7ac75d3db018bab6c1ed9697a8db6d5681
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-14 09:41:37 +0000

    file created

---
+++ P_06/include/node.h
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file node.h
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of the class Node.
+ * @version 0.1
+ * @since Mar 14 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+class Node {
+
+};
