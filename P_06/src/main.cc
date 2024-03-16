bab365d0b87d5562063d1ab6080e5072dcb6e950
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-16 17:53:16 +0000

    done

--- P_06/src/main.cc
+++ P_06/src/main.cc
@@ -23,13 +23,13 @@ int main(int argc, char* argv[]) {
   try {
     srand(time(NULL));
     int nodes = 0;
-    int time_limit = 5;
-    if (argc != 2 && argc != 3 && argc != 4) {
+    float time_limit = 5.f * 60000.f;
+    if (argc != 2 && argc != 3 && argc != 4 && argc != 6) {
       std::cerr << "Usage: " << argv[0] << " <files_directory> <-t [time_limit]> <-g [files to generate]>" << std::endl;
       throw std::invalid_argument("Invalid number of arguments");
     } else if (argc == 4) {
       if (std::string(argv[2]) == "-t") {
-        time_limit = std::stoi(argv[3]);
+        time_limit = std::stof(argv[3]) * 60000.f;
       } else if (std::string(argv[2]) == "-g") {
         nodes = std::stoi(argv[3]);
         for (int i = 0; i < 3; i++) {
@@ -38,7 +38,7 @@ int main(int argc, char* argv[]) {
       }
     } else if (argc == 6) {
       if (std::string(argv[2]) == "-t") {
-        time_limit = std::stoi(argv[3]);
+        time_limit = std::stof(argv[3]) * 60000.f;
       } else if (std::string(argv[2]) == "-g") {
         nodes = std::stoi(argv[3]);
         for (int i = 0; i < 3; i++) {
@@ -46,7 +46,7 @@ int main(int argc, char* argv[]) {
         }
       }
       if (std::string(argv[4]) == "-t") {
-        time_limit = std::stoi(argv[5]);
+        time_limit = std::stof(argv[5]) * 60000.f;
       } else if (std::string(argv[4]) == "-g") {
         nodes = std::stoi(argv[5]);
         for (int i = 0; i < 3; i++) {
@@ -57,19 +57,20 @@ int main(int argc, char* argv[]) {
     std::string files_directory = argv[1];
     std::vector<std::string> file_names;
     for (const auto& entry : std::filesystem::directory_iterator(files_directory)) {
-      file_names.emplace_back(entry.path());
+      file_names.emplace_back(entry.path().string());
     }
     std::cout << "Instancia\tValor F.B.\tTiempo F.B. (ms)\tValor P.D.\tTiempo P.D. (ms)\t"
                  "Valor Greedy\tTiempo Greedy (ms)" << std::endl;
+    write(0, "0", 0);
     for (const auto& file_name : file_names) {
       std::vector<std::string> file_content = ReadFile(file_name);
       Graph* graph = CreateGraph(file_content);
-      std::cout << file_name << "\t";
-      // ! ?????
-      std::cout << file_name.size() << "\t";
+      std::cout << file_name << "      ";
       CalculateTimes(graph, time_limit);
-      std::cout << "\n";
     }
+    std::cout << std::endl;
+    std::cout << "All instances have been solved.\nPress Ctrl + C to exit." << std::endl;
+    std::this_thread::sleep_for(std::chrono::hours(24));
     return 0;
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
