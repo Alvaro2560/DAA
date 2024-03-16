d5a48fa57f6edd496b39b72ff0bfef95aed81eec
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-16 17:53:01 +0000

    done

--- P_06/include/solver.h
+++ P_06/include/solver.h
@@ -18,11 +18,6 @@
 
 #include "graph.h"
 
-#include <future>
-#include <chrono>
-#include <thread>
-#include <iostream>
-
 typedef std::pair<std::vector<Node*>, int> Solution;
 
 /**
@@ -31,39 +26,8 @@ typedef std::pair<std::vector<Node*>, int> Solution;
 class Solver {
   public:
     Solver(void) = default;
-    Solution Solve(Graph* graph, const int& time_limit, std::string& time) {
-        auto start = std::chrono::steady_clock::now();
-
-        // Iniciar la tarea asíncrona
-        std::future<Solution> future = std::async(std::launch::async, [this, graph] {
-            return this->RunAlgorithm(graph);
-        });
-
-        bool timeout = false;
-
-        // Esperar hasta que la tarea se complete o el tiempo límite expire
-        std::future_status status = future.wait_for(std::chrono::minutes(time_limit));
-        
-        if (status == std::future_status::timeout) {
-            // Si la tarea excede el límite de tiempo, cancelarla
-            future.get();
-            timeout = true;
-        }
-
-        // Calcular el tiempo transcurrido
-        auto end = std::chrono::steady_clock::now();
-        std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        time = std::to_string(duration.count()); // Convertir a string para almacenar el tiempo
-
-        // Manejar el caso de tiempo excedido
-        if (timeout) {
-            time = "EXCESIVO";
-            return Solution();
-        }
-
-        // Obtener el resultado de la tarea
-        return future.get();
-    
+    Solution Solve(Graph* graph) {
+      return RunAlgorithm(graph);
     }
     ~Solver(void) = default;
   protected:
