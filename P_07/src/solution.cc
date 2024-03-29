37314ca61fc6cca7531adf46e6f593ea528ac5db
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-29 19:05:33 +0000

    file created

---
+++ P_07/src/solution.cc
@@ -0,0 +1,15 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file solution.cc
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Definition of class Solution.
+ * @version 0.1
+ * @since Mar 29 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
