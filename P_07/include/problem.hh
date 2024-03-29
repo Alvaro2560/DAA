13710d85c2211cdd12dda6de6c0e1bdd2ffcb4bc
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-03-29 16:38:18 +0000

    first version

---
+++ P_07/include/problem.hh
@@ -0,0 +1,40 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Diseño y Análisis de Algoritmos 2023-2024
+ * 
+ * @file problem.hh
+ * @author Álvaro Fontenla León (alu0101437989@ull.edu.es)
+ * @brief Declaration of class Problem.
+ * @version 0.1
+ * @since Mar 29 2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+/**
+ * @class Problem
+ * @brief Class that represents a problem instance.
+ */
+class Problem {
+  public:
+    Problem(const std::string& file_name);
+    friend std::ostream& operator<<(std::ostream& os, const Problem& problem);
+    ~Problem(void);
+  private:
+    void ReadFile(const std::string& file_name);
+  private:
+    int tasks_;
+    int machines_;
+    std::vector<int> processing_times_;
+    std::vector<std::vector<int>> preparation_times_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Problem& problem);
