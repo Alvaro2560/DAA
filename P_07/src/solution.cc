cc2b41479a561115315536b0fb7e7335b20a0465
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-04-01 19:50:01 +0100

    commented

--- P_07/src/solution.cc
+++ P_07/src/solution.cc
@@ -17,20 +17,48 @@
 #include "../include/solution.hh"
 #include "../include/task.hh"
 
+/**
+ * @brief Construct a new Solution::Solution object
+ * 
+ * @param machines Number of machines.
+ */
 Solution::Solution(const int& machines) : tasks_(machines), tcts_(machines, 0) { }
 
+/**
+ * @brief Add a task to a machine.
+ * 
+ * @param machine Machine's id.
+ * @param task Task to add.
+ */
 void Solution::addTask(const int& machine, Task* task) {
   tasks_[machine].emplace_back(task);
 }
 
+/**
+ * @brief Get the tasks of a machine.
+ * 
+ * @param machine Machine's id.
+ * @return std::vector<Task*> Tasks of the machine.
+ */
 std::vector<Task*> Solution::getTasks(const int& machine) const {
   return tasks_[machine];
 }
 
+/**
+ * @brief Get the last task of a machine.
+ * 
+ * @param machine Machine's id.
+ * @return Task* Last task of the machine.
+ */
 Task* Solution::getLastTask(const int& machine) {
   return tasks_[machine].back();
 }
 
+/**
+ * @brief Get the total completion time.
+ * 
+ * @return int Total completion time.
+ */
 int Solution::getTCT(void) const {
   int tct = 0;
   for (size_t i = 0; i < tcts_.size(); i++) {
@@ -39,10 +67,21 @@ int Solution::getTCT(void) const {
   return tct;
 }
 
+/**
+ * @brief Get the total completion time of a machine.
+ * 
+ * @param machine Machine's id.
+ * @return int Total completion time of the machine.
+ */
 int Solution::getTCT(const int& machine) const {
   return tcts_[machine];
 }
 
+/**
+ * @brief Sum the total completion time of a machine.
+ * 
+ * @param machine Machine's id.
+ */
 void Solution::sumTCT(const int& machine) {
   for (size_t i = 0; i < tasks_[machine].size(); i++) {
     if (i == 0) {
@@ -53,6 +92,13 @@ void Solution::sumTCT(const int& machine) {
   }
 }
 
+/**
+ * @brief Overload of the operator <<.
+ * 
+ * @param os Output stream.
+ * @param solution Solution to print.
+ * @return std::ostream& Output stream.
+ */
 std::ostream& operator<<(std::ostream& os, const Solution& solution) {
   for (size_t i = 0; i < solution.tasks_.size(); ++i) {
     os << "Machine " << i + 1 << ": ";
