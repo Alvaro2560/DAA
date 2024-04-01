1a83c43dde231b3168d6b801f6ec5a35fb3abb2a
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-04-01 19:22:56 +0100

    added vector atribute for tcts

--- P_07/src/solution.cc
+++ P_07/src/solution.cc
@@ -17,7 +17,7 @@
 #include "../include/solution.hh"
 #include "../include/task.hh"
 
-Solution::Solution(const int& machines) : tasks_(machines) { }
+Solution::Solution(const int& machines) : tasks_(machines), tcts_(machines, 0) { }
 
 void Solution::addTask(const int& machine, Task* task) {
   tasks_[machine].emplace_back(task);
@@ -31,13 +31,36 @@ Task* Solution::getLastTask(const int& machine) {
   return tasks_[machine].back();
 }
 
+int Solution::getTCT(void) const {
+  int tct = 0;
+  for (size_t i = 0; i < tcts_.size(); i++) {
+    tct += tcts_[i];
+  }
+  return tct;
+}
+
+int Solution::getTCT(const int& machine) const {
+  return tcts_[machine];
+}
+
+void Solution::sumTCT(const int& machine) {
+  for (size_t i = 0; i < tasks_[machine].size(); i++) {
+    if (i == 0) {
+      tcts_[machine] += tasks_[machine][i]->getPreparationTime(0) + tasks_[machine][i]->getProcessingTime();
+    } else {
+      tcts_[machine] += tasks_[machine][i]->getPreparationTime(tasks_[machine][i - 1]->getId()) + tasks_[machine][i]->getProcessingTime();
+    }
+  }
+}
+
 std::ostream& operator<<(std::ostream& os, const Solution& solution) {
   for (size_t i = 0; i < solution.tasks_.size(); ++i) {
-    os << "Machine " << i << ": ";
+    os << "Machine " << i + 1 << ": ";
     for (size_t j = 0; j < solution.tasks_[i].size(); ++j) {
       os << solution.tasks_[i][j]->getId() << " ";
     }
-    os << std::endl;
+    os << "\nTCT: " << solution.tcts_[i] << std::endl;
   }
+  os << "Total TCT: " << solution.getTCT() << std::endl;
   return os;
 }
